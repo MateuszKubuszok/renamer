9f6dd80f52461280acee7d2ced3f561217dd5605
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2025-07-20 20:36:07 +0200

    Initial commit

---
+++ app.scala
@@ -0,0 +1,5 @@
+package renamer
+
+@main def main = {
+  println("Hello, World!")
+}
---
+++ project.scala
@@ -0,0 +1,3 @@
+//> using scala 3.7.1
+//> using dep com.olvind.tui::tui::0.0.7
+//> using dep "com.softwaremill.ox::core:1.0.0-RC1"
